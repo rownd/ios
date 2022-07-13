b2848112adface1cf620b955c54db35ba4ef8209
Matt Hamann <mhamann@rownd.io>
2022-07-13 16:38:23 -0400

    fix(release): specify dependency list

--- Package.swift
+++ Package.swift
@@ -24,23 +24,31 @@ let package = Package(
     
     dependencies: [
         .package(
+            name: "AnyCodable",
             url: "https://github.com/Flight-School/AnyCodable",
-            from: "0.6.0"
+            .upToNextMajor(from: "0.6.0")
         ),
         .package(
+            name: "ReSwift",
             url: "https://github.com/ReSwift/ReSwift",
-            from: "6.1.0"
+            .upToNextMajor(from: "6.1.0")
         ),
         .package(
+            name: "ReSwift-Thunk",
+            url: "https://github.com/ReSwift/ReSwift-Thunk",
+            .upToNextMajor(from: "2.0.0")
+        ),
+        .package(
+            name: "JWTDecode",
             url: "https://github.com/auth0/JWTDecode.swift",
-            from: "2.6.3"
+            .upToNextMajor(from: "2.6.3")
         )
     ],
     
     targets: [
         .target(
             name: "Rownd",
-            dependencies: []
+            dependencies: ["AnyCodable", "ReSwift", "ReSwift-Thunk", "JWTDecode"]
         )
     ],
     
---
+++ Sources/Rownd/framework/Encodable.swift
@@ -0,0 +1,8 @@
+//
+//  Encodable.swift
+//  RowndSDK
+//
+//  Created by Matt Hamann on 7/13/22.
+//
+
+import Foundation
