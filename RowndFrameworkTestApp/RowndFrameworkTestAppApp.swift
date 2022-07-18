6289d426408c15ad86cd9e93e940ff1cbd7480aa
Matt Hamann <mhamann@rownd.io>
2022-07-18 12:01:58 -0400

    feat(encryption): initial api + tests (#1)

---
+++ RowndFrameworkTestApp/RowndFrameworkTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RowndFrameworkTestAppApp.swift
+//  RowndFrameworkTestApp
+//
+//  Created by Matt Hamann on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct RowndFrameworkTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
