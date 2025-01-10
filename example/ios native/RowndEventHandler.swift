fe6cc3f16f10a779e0cafa8288b9dec4c1a2d677
Matt Hamann <mhamann@rownd.io>
2025-01-10 09:29:32 -0500

    chore(test): throw when no token is present after sign in

--- example/ios native/RowndEventHandler.swift
+++ example/ios native/RowndEventHandler.swift
@@ -8,12 +8,31 @@
 import Foundation
 import Rownd
 
+struct EventError: Error, CustomStringConvertible {
+    var message: String
+
+    init(_ message: String) {
+        self.message = message
+    }
+
+    public var description: String {
+        return message
+    }
+}
+
 class RowndEventHandler: RowndEventHandlerDelegate {
     func handleRowndEvent(_ event: RowndEvent) {
         switch event.event {
         case .signInCompleted:
             let userType = event.data?["user_type"]
             let appVariantUserType = event.data?["app_variant_user_type"]
+            Task {
+                let token = try await Rownd.getAccessToken()
+
+                if token == nil {
+                    throw EventError("Token unexpectedly nil")
+                }
+            }
             break
 
         default:
